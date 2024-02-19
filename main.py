f458ed69df4949506c70ac68402009530e3c7049
dudelim <137728498+dudelim@users.noreply.github.com>
2024-02-19 17:07:43 +0000

    Update

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ def str_endereco(endereco):
   parte_endereco = endereco.split()  # Divide o endereço
   nome_rua = ''
   numero_rua = ''
+
+for i, parte in enumerate(parte_endereco):
+  if parte.isdigit() or (parte[0].isdigit() and i > 0 and not parte_endereco[i-1].isdigit()):  # Espaço núm/letra
+      if numero_rua and not parte.isdigit():  
+          numero_rua += parte  
